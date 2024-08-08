91f07b6189b574653f820e5abbab213207296db9
sabbir ahmmed <ahsabbir103@gmail.com>
2024-08-08 12:34:35 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { FoundersGrotesk } from "@/lib/fonts";
+import { FoundersGrotesk, FoundersGroteskXCond } from "@/lib/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className=" max-w-7xl mx-auto flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className={` ${FoundersGrotesk.className} text-zinc-800 font-FoundersGrotesk font-bold text-[22vh] leading-[15vh] uppercase `}>together we make a difference</h1>
+        <h1 className={` ${FoundersGroteskXCond.className} text-zinc-800 font-FoundersGroteskXCond font-bold text-[22vh] leading-[15vh] uppercase `}>together we make a difference</h1>
       </div>
     </main>
   );
